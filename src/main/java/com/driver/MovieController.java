dd496abfc6997388776214391423be7065e9882a
Mausam Raj <128462297+Mausam1815@users.noreply.github.com>
2023-11-05 16:55:20 +0530

    Done 4

--- src/main/java/com/driver/MovieController.java
+++ src/main/java/com/driver/MovieController.java
@@ -51,7 +51,7 @@ public class MovieController {
     }
 
     @GetMapping("/get-all-movies")
-    public ResponseEntity<List<String>> getAllMovies() {
+    public ResponseEntity<List<String>> findAllMovies() {
         return movieService.getAllMovies();
     }
 
